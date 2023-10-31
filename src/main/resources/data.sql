1d55bfac9a023a510e748d7db35603c0f6c423a9
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-10-31 15:19:15 +0200

    fix

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,8 +7,8 @@ values
 
 INSERT INTO users (id, email, first_name, last_name, image_url, is_active, password)
 VALUES
-    (1, 'admin@example.com', 'Admin', 'Adminov', null, 1, '57e7759fd2d59275fc3c3cd5dd2ace5013b39ee972999412f3f5f5c3382b6765c2571ef86648abe2'),
-    (2, 'user@example.com', 'User', 'Userov', null, 1, '57e7759fd2d59275fc3c3cd5dd2ace5013b39ee972999412f3f5f5c3382b6765c2571ef86648abe2');
+    (1, 'admin@example.com', 'Admin', 'Adminov', null, true, '57e7759fd2d59275fc3c3cd5dd2ace5013b39ee972999412f3f5f5c3382b6765c2571ef86648abe2'),
+    (2, 'user@example.com', 'User', 'Userov', null, true, '57e7759fd2d59275fc3c3cd5dd2ace5013b39ee972999412f3f5f5c3382b6765c2571ef86648abe2');
 
 
 INSERT INTO users_user_roles (user_entity_id, user_roles_id)
